003332d62abcf89920233e6b49e83d9fae07e07b
carlonchosando <cdscapitolio@gmail.com>
2023-02-19 21:10:46 -0300

    En la rama "main"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Word</h1>
-      <p>Aqui armando algo de texto</p>
+      <p>Aqui armando algo de texto para continuidad de la APP</p>
     </div>
   )
 }
