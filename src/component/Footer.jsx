a863e807e073dfad760cfd3ee233d576aeb5888d
carlonchosando <you@example.com>
2023-02-22 13:16:17 -0300

    Changes to be committed:

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
     return (
         <footer>
             <h1>Desde Pie De Pagina</h1>
+            <p>esta es una APP para los turnos de la Veterinaria</p>
         </footer>
     )
 }
