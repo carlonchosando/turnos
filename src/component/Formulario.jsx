05619ba9037a8d9cf2f5d683271560793fa5dd41
carlonchosando <you@example.com>
2023-04-11 23:43:04 -0300

    fin de la practica

--- src/component/Formulario.jsx
+++ src/component/Formulario.jsx
@@ -2,18 +2,57 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div className='md:w-1/2 lg:w-2/5'>      
+    <div className='md:w-1/2 lg:w-2/5'>
       <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
-      <p className='text-lg mt-5 text-center'>
+      <p className='text-lg mt-5 mb-10 text-center'>
         Añade Pacientes y {' '}
         <span className='text-indigo-600 font-bold'>Administralos</span>
       </p>
-      <form className='bg-white shadow-md'>
-         <div> 
-             <label className='' name="nombre">Nombre de la Mascota</label>
-             <input 
-             type='text' className='rounded' name='nombre' placeholder='Nombre de la Mascota'/>
-         </div>
+      <form className='bg-white shadow-lg rounded-lg py-10 px-5 mt-10'>
+        <div className='mb-5'>
+          <label
+            htmlFor='nombre'
+            className='block text-gray-700 uppercase font-bold'>
+            Nombre de la Mascota
+          </label>
+          <input
+            id='nombre'
+            type='text'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'            
+            placeholder='Nombre de la Mascota' />
+        </div>
+
+        <div className='mb-5'>
+          <label
+            htmlFor='alta'
+            className='block text-gray-700 uppercase font-bold'>
+            Alta
+          </label>
+          <input
+            id='alta'
+            type='date'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label
+            htmlFor='sintomas'
+            className='block text-gray-700 uppercase font-bold'>
+            Síntomas
+          </label>
+          <textarea
+            id='sintomas'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            placeholder='Describe los síntomas'
+          />          
+        </div>
+
+        <input 
+          type='submit'
+          className='bg-indigo-600 w-full p-3 text-white uppercase font-bold rounded-md hover:bg-indigo-800 cursor-pointer transition-all'
+          value='Agregar Paciente'
+        />
       </form>
     </div>
   )
--- src/component/ListadoPacientes.jsx
+++ src/component/ListadoPacientes.jsx
@@ -4,6 +4,11 @@ const ListadoPacientes = () => {
   return (
     <div className='md:w-1/2 lg:w-3/5' >
       <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className='text-lg mt-5 mb-10 text-center'>
+        Administra tus {' '}
+        <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+
+      </p>
     </div>
   )
 }
